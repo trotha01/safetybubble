05f7e77529595ce5ab5748fcea26b8e656548aaf
trotha01 <trevor.rothaus@sendgrid.com>
2018-02-01 10:55:46 -0600

    add travis for gh-pages

---
+++ gh-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# compile
+elm make Main.elm --yes --output index.html
+
+# remove index.html from .gitignore
+# compatible with both GNU and BSD/Mac sed
+sed -i.bak '/index.html/d' .gitignore
+rm .gitignore.bak
+
+echo "ls"
+ls
+
+# init branch and commit
+git init
+git config user.name "Trevor Rothaus (via Travis CI)"
+git config user.email "trotha01@gmail.com"
+git checkout -b gh-pages
+git add .
+git status
+git commit -m "Deploy to GitHub Pages [skip ci]"
+git push --force "https://${GITHUB_TOKEN}@github.com/trotha01/safetybubble.git" gh-pages
